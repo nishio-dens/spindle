fbf8c69880a241641d1e440ce08fb7e9c6ecc01e
Shinsuke Nishio <nishio@densan-labs.net>
2017-07-03 01:00:24 +0900

    create drawer

--- app/src/lib/drawer/drawer.js
+++ app/src/lib/drawer/drawer.js
@@ -6,10 +6,13 @@ export default class Drawer extends React.Component {
 
     this.canvasWidth = 300
     this.canvasHeight = 300
+    this.currentImage = null
+    this.images = null
   }
 
   componentDidMount() {
     this.drawerCanvas = document.getElementById('drawer-canvas')
+    this.canvasContext = this.drawerCanvas.getContext('2d')
   }
 
   updateWindowSize(width, height) {
@@ -21,16 +24,46 @@ export default class Drawer extends React.Component {
     this.drawImage()
   }
 
-  loadImage(imagePath) {
+  // Preload Images
+  // @params image
+  // [{ key: "KEY", path: Path }]
+  preloadImages(imgs) {
+    if (this.images === null) {
+      this.images = {}
+    }
+    for (let v of imgs) {
+      let img = new Image()
+      img.path = img
+      this.images[v.key] = {
+        path: v.path,
+        image: image,
+        loaded: false
+      }
+      img.onload = () => {
+        this.images[v.key].loaded = true
+      }
+    }
   }
 
-  preloadImages(imagePaths) {
+  clearPreloadImages() {
+    this.images = null
   }
 
-  clearImages() {
+  drawImage(key) {
+    let image = this.images[key]
+    if (!image) {
+      console.log("Image Not Found " + key)
+      return
+    }
+
+    this.currentImage = image
+    this.drawImage()
   }
 
   drawImage() {
+    if (this.currentImage) {
+      this.canvasContext.drawImage(image.image, 0, 0, 300, 300)
+    }
     console.log("DRAW " + this.canvasWidth + " " + this.canvasHeight)
   }
 
